23dfe42df7e0086d8a466f406bf035f68b0571f4
Monroy <kevinmonroy17@augustana.edu>
2018-10-15 16:46:55 -0500

    change way to draw in canvas

--- Fractals/src/fractalcircles/FractalCircle.java
+++ Fractals/src/fractalcircles/FractalCircle.java
@@ -3,6 +3,7 @@ package fractalcircles;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
@@ -18,6 +19,7 @@ public class FractalCircle extends Application{
 
 	private static Pane root = new Pane();
 	private static Canvas canvas;
+	private static GraphicsContext g;
 	private static Ellipse ellipse;
 	private static Line line;
 	
@@ -27,12 +29,14 @@ public class FractalCircle extends Application{
 	
 	@Override
 	public void start(Stage stage) {
-		canvas = new Canvas(600,600);
+		canvas = new Canvas(640,360);
+		g = canvas.getGraphicsContext2D();
 		root.getChildren().add(canvas);
 		Scene scene = new Scene(root,Color.WHITE);
 		stage.setScene(scene);
 		
-		drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, canvas.getWidth()/2, 1);
+		drawFourFractalCircle(canvas.getWidth()/2,canvas.getHeight()/2,canvas.getWidth()/2,canvas.getWidth()/2);
+//		drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, canvas.getWidth()/2, canvas.getHeight()/2);
 //		cantorFractal(10,50,canvas.getWidth()-20);
 		
 		stage.show();
@@ -68,28 +72,37 @@ public class FractalCircle extends Application{
 	 * @param radius - radius of the ellipse
 	 * @param level - will be used to change color after each level.
 	 */
-	static void drawCircle(double x, double y, double radius, int level) {
-		ellipse = new Ellipse(x,y,radius ,radius);
-		ellipse.setFill(null);
-		ellipse.setStroke(Color.BLUE);
-//		ellipse.setStroke(new Color(0.1,0,0,0.8));
-		ellipse.setStrokeWidth(1);
-		root.getChildren().add(ellipse);
+	static void drawFourFractalCircle(double x, double y, double radiusX, double radiusY) {
+//		ellipse = new Ellipse(x,y,radius ,radius);
+//		ellipse.setFill(null);
+//		ellipse.setStroke(Color.BLUE);
+//		ellipse.setStrokeWidth(1);
 		
-		if(radius > 8) {
-			//		    drawCircle(d, e, radius);
-			//AS PROFESSOR STONEDAHL ABOUT THE VITALITY OF THE 0.5F
-		    radius *= 0.75;
-		    
-		    drawCircle(x - radius/2, y, radius/2, level + 1);
-		    drawCircle(x + radius/2, y, radius/2, level+ 1);
-		    drawCircle(x, y - radius/2, radius/2, level + 1);			
-		    drawCircle(x, y + radius/2, radius/2,level + 1);
+		//root.getChildren().add(ellipse);
+		g.strokeOval(x-radiusX,y-radiusY,radiusX*2,radiusY*2);
+		
+		if(radiusX > 2) {
+//		    radiusX *= 0.75;
+//		    radiusY *= 0.75;
+		    drawCircle(x + radiusX / 2, y, radiusX/2, radiusY/2);
+		    drawCircle(x - radiusX / 2, y, radiusX/2, radiusY/2);
+		   
+//		    drawCircle(x, y - radiusY, radiusX/2, radiusY/2);			
+//		    drawCircle(x, y + radiusY, radiusX/2, radiusY/2);
 		}
 	}
 	
 	static void drawHtree() {
 		
+	}
+//	
+	static void drawCircle(double x, double y, double radiusX, double radiusY) {
+		g.strokeOval(x-radiusX,y-radiusY,radiusX*2,radiusY*2);
+		if (radiusX>2) {
+			radiusX *= 0.75;
+			radiusY *= 0.75;
+			drawCircle(x, y, radiusX, radiusY);
+		}
 	}
 }
 
